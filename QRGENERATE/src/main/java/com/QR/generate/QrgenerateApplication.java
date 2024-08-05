a0905fd090b6f1e9ae1160993c4692875f360ea1
DasariMahesh12345 <131242450+DasariMahesh12345@users.noreply.github.com>
2024-08-05 13:32:29 +0530

    Add files via upload

---
+++ QRGENERATE/src/main/java/com/QR/generate/QrgenerateApplication.java
@@ -0,0 +1,13 @@
+package com.QR.generate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrgenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrgenerateApplication.class, args);
+	}
+
+}
---
+++ QRGENERATE/src/test/java/com/QR/generate/QrgenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QR.generate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrgenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
